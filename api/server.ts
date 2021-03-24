3f6c5e461114cc7003020040bf1b24e831f3d9f7
Taylor Sweetman <taylor.sweetman@gmail.com>
2021-03-23 22:01:58 -0400

    got cors policy working, can api call from vue now

--- api/server.ts
+++ api/server.ts
@@ -1,26 +1,24 @@
-import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
 
-import * as home from "./controllers/home.controller";
-import * as home2 from "./controllers/home.controller_copy";
+import * as home from "./controllers/home.controller"
+import * as home2 from "./controllers/home.controller_copy"
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.set("port", process.env.SERVER_PORT || 3000);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", home.index);
-app.get("/test", home2.index);
+app.set("port", process.env.SERVER_PORT || 3000)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.get("/", home.index)
+app.get("/test", home2.index)
 
 app.listen(app.get("port"), () => {
-  console.log(
-    "App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
-});
+  console.log("App is running at http://localhost:%d in %s mode", app.get("port"), app.get("env"))
+})
 
-export default app;
+export default app
